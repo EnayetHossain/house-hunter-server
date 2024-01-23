dafbfaa5251c2b56b2a24e3c9e013c743d15bb4c
EnayetHossain <enayethossasin135@gmail.com>
2024-01-23 13:17:21 +0600

    update house information

--- controllers/house.controller.js
+++ controllers/house.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const CustomError = require("../errors/customError.error");
 const House = require("../models/house.model");
 
@@ -56,6 +57,68 @@ const addHouse = async (req, res) => {
   }
 };
 
+// update a house in database
+const updateHouse = async (req, res) => {
+  const {
+    name,
+    address,
+    city,
+    bedRooms,
+    bathRooms,
+    roomSize,
+    picture,
+    availabilityDate,
+    rentPerMonth,
+    phoneNumber,
+    description
+  } = req.body;
+
+  const id = req.params.id;
+  console.log(id);
+
+  if (
+    !name ||
+    !address ||
+    !city ||
+    !bedRooms ||
+    !bathRooms ||
+    !roomSize ||
+    !picture ||
+    !availabilityDate ||
+    !rentPerMonth ||
+    !phoneNumber ||
+    !description
+  )
+    throw new CustomError("All fields are required", 400);
+
+  try {
+    const filter = { _id: new mongoose.Types.ObjectId(id) };
+    const updateDoc = {
+      name,
+      address,
+      city,
+      bedRooms,
+      bathRooms,
+      roomSize,
+      picture,
+      availabilityDate,
+      rentPerMonth,
+      phoneNumber,
+      description
+    };
+
+    const updatedHouse = await House.findOneAndUpdate(filter, updateDoc, {
+      new: true
+    });
+
+    res.status(201).json({ status: "Success", updatedHouse });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "Failed", error });
+  }
+};
+
 module.exports = {
-  addHouse
+  addHouse,
+  updateHouse
 };
--- routes/house.route.js
+++ routes/house.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { addHouse } = require("../controllers/house.controller");
+const { addHouse, updateHouse } = require("../controllers/house.controller");
 
 const router = express.Router();
 
 router.route("/add-house").post(addHouse);
+router.route("/update-house/:id").patch(updateHouse);
 
 module.exports = router;
